08b9911886ab1f7b6b74bd913d1183c3a056bd11
Emediong231 <diamsampson@gmail.com>
2025-09-12 02:05:24 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'knight Bot', // Your name
+  botOwner: 'Diamond Bot', // Your name
   ownerNumber: '2348126775174', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
